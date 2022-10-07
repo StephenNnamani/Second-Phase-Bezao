3e83a5bf46454aa6002f835941e9ce8be41baadb
StephenNnamani <sir.stephennnamani@gmail.com>
2022-10-07 11:06:40 +0100

    Updated The GuessGames

--- GuessGame/GuessGame/Program.cs
+++ GuessGame/GuessGame/Program.cs
@@ -10,7 +10,7 @@ namespace GuessGame
     {
         static void Main(string[] args)
         {
-           Random random = new Random();
+            Random random = new Random();
 
             bool playAgain = true;  // Checkes if you want to play again
 
@@ -34,6 +34,9 @@ namespace GuessGame
             //Secret Number
 
             int SecretNumber;
+
+            //Logic of whether a play won or lost
+            string winORlose = "You Lost";
 
             //-------------------------------------------------------
 
@@ -89,29 +92,96 @@ Each attempt tells you if you are higher or lower than the secret number. and th
                         Console.WriteLine("The Secret Number is: " + SecretNumber);
                         Console.WriteLine("YOU WIN!");
                         Console.WriteLine("Number of Tries: " + count);
+                        winORlose = "You WON!!!";
                     }
                 }
                 
 
-
-                Console.WriteLine("Would you like to play again (Y/N): ");
-                response = Console.ReadLine();
-                response = response.ToUpper();
-
-                if (response == "Y")
-                {
-                    playAgain = true;
-                }
-                else
-                {
-                    playAgain = false;
-                }
+                do{
+                        Console.WriteLine("Would you like to play again (Y/N): ");
+                        response = Console.ReadLine();
+                        response = response.ToUpper();
+                        if (response == "Y")
+                        {
+                             playAgain = true;
+                            break;
+                        }
+                        else if (response == "N")
+                        {
+                             playAgain = false;
+                            break;
+                        } else{
+                            Console.WriteLine("Please type in the correct Character Y or N");
+                        }
+                }while(response != "Y" || response != "N");
             }
                 Console.WriteLine("Thanks for playing! ... I guess");
 
-                Console.ReadKey();
+            Users newUser = new Users();
+
+            Console.Write("Type in your First name: ");
+            var firstName = newUser.firstName;
+            firstName = Console.ReadLine();
+
+            Console.Write("Type in your Last name: ");
+            var lastName = newUser.lastName;
+            lastName = Console.ReadLine();
+
+            Console.Write("Type in your age: ");
+            var age = newUser.age;
+            age = Convert.ToInt64(Console.ReadLine());
+         
+            Console.Write("Type in your height: ");
+            var height = newUser.height;
+            height = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Type in your Skin Color: ");
+            var complexion = newUser.complexion;
+            complexion = Console.ReadLine();
+
+            Console.Write("Type in your State: ");
+            var state = newUser.state;
+            state = Console.ReadLine();
+
+            Console.Write("Type in your Country: ");
+            var Country = newUser.Country;
+            Country = Console.ReadLine();
+
+             Console.WriteLine($@"
+My name is: {firstName} {lastName}, and I am from {state}.
+my age is: {age} and I am {complexion} in complexion, with a height of {height}cm. I am from {Country} and my state of Birth is {state}. and {winORlose} the game
+Thank You");
+
+            currency();
+           
+            Console.ReadKey();
             
             }
+        private static void currency(){
+            Console.WriteLine("{0:C}", 123.34, "\n");
+            Console.WriteLine("\n==============================================================================\n");
+            DateTime myBirthday = DateTime.Parse("02/11/1991");
+            TimeSpan daysOnEarth = DateTime.Now.Subtract(myBirthday);
+
+
+            Console.WriteLine("\n==============================================================================\n");
+
+            DateTime today = DateTime.Today;
+            Console.WriteLine(today.ToLongDateString());
+
+            Console.WriteLine(daysOnEarth);
+
 
         }
     }
+    class Users
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public long age { get; set; }
+        public string complexion { get; set; }
+        public Double height { get; set; }
+        public string Country { get; set; }
+        public string state { get; set; }
+    }
+}
