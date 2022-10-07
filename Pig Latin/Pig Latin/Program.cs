c37dc59d86db28797922cdb848abe37882187c47
StephenNnamani <sir.stephennnamani@gmail.com>
2022-10-07 16:37:48 +0100

    Updated Pig Latin

--- Pig Latin/Pig Latin/Program.cs
+++ Pig Latin/Pig Latin/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace Pig_Latin
 {
@@ -21,37 +22,67 @@ namespace Pig_Latin
             while (translateAgain)
             {
                 string ay = "ay";
-
-                Console.WriteLine("Type in the words you want to Translate here: \n \n");
-                Comments = Console.ReadLine();
-
-
-                foreach (string letters in Comments.Split(' '))
-                {
-                    string firstLetter = letters.Substring(0, 1);
-                    //Console.WriteLine(firstLetter);
-                    string restOfWord = letters.Substring(1, letters.Length - 1);
-
-                    string Converted = restOfWord+firstLetter+ay + " ";
-
-                    var lowerCase = Converted.ToLower();
-                    var r = new Regex(@"(^[a-z])|\.\s+", RegexOptions.ExplicitCapture);
-                    var result = r.Replace(lowerCase, s => s.Value.ToUpper());
-                    Console.Write(result);
-
-                }
-
-                Console.WriteLine(@"
-===========================================================
-Do you want to translate this word back to English? (Y/N)
-===========================================================
-");
-                string answer = Console.ReadLine();
                 
-                if (answer == "Y" || answer == "y")
+
+                Console.WriteLine("English to Pig Latin = Press 1 \nPig Latin to English = Press 2");
+                string T = Console.ReadLine();
+
+                if (T == "1")
                 {
-                    Console.WriteLine(Comments);
+                    Console.WriteLine("Type in the English words you wish to translate back to Pig Latin: ");
+                    Comments = Console.ReadLine();
+                    foreach (string letters in Comments.Split(' '))
+                    {
+                        string firstLetter = letters.Substring(0, 1);
+                    
+                        string restOfWord = letters.Substring(1, letters.Length - 1);
+
+                        string Converted = restOfWord+firstLetter+ay + " ";
+
+                        var lowerCase = Converted.ToLower();
+                    
+
+                        var patterns = @"(^[a-z,.;?])|\.\s+";
+                        var r = new Regex(patterns, RegexOptions.ExplicitCapture);
+
+                        // Creates a TextInfo based on the "en-US" culture.
+                        TextInfo myTI = new CultureInfo("en-US",false).TextInfo;
+                        var result = r.Replace(lowerCase, s => s.Value.ToUpper());
+                        Console.Write(myTI.ToTitleCase(result));
+                    }
+                
                 }
+                else if(T == "2")
+                {
+                    string latinCapture;
+                    Console.WriteLine("Type in the latin words you wish to translate back to English: ");
+                    latinCapture = Console.ReadLine();
+                    foreach (string letters in latinCapture.Split(' '))
+                    {
+                        //Capturing the last three letters of each word
+                        string lastThree = letters.Substring(letters.Length-3);
+                       // Console.WriteLine(lastThree);
+                        string firstLetter;
+
+                        foreach(char letter in lastThree.IndexOf(0))
+                        {
+                            firstLetter = Char.ToString(letter);
+                            Console.WriteLine(firstLetter);
+                        } 
+
+                        string restOfWord = letters.Substring(0, letters.Length - 3);
+
+                      // string Converted = firstLetter+restOfWord + " ";
+
+                       //Console.WriteLine(Converted);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please select the RIGHT option i.e either 1 or 2");
+                }
+            }
+                
 
                 Console.WriteLine("\n =============================================================================================");
                 Console.WriteLine("Do you want to translate again?(Y/N): ");
@@ -66,9 +97,9 @@ Do you want to translate this word back to English? (Y/N)
                 {
                     translateAgain = true;
                 }
-            }
-            Console.WriteLine("Press any key to terminate");
-            Console.ReadKey();
+            
+                Console.WriteLine("Press any key to terminate");
+                Console.ReadKey();
         }
     }
 }
